f741d6c85b8a0c43c885fd4273d294d7ab83fdc4
Takashi Saito <takashi11171117@gmail.com>
2021-05-17 20:30:29 +0900

    add videos migration

---
+++ database/migrations/2021_05_17_110448_create_videos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('thumbnail_path');
+            $table->bigInteger('teacher_id')->unsigned()->index();
+            $table->string('vimeo_video_id');
+            $table->timestamps();
+
+            $table->foreign('teacher_id')->references('id')->on('teachers')->onDelete('cascade');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
